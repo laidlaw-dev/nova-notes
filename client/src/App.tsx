0abd1bdd89c81a338620710bbbad1fe8fb882eb6
laidlaw_dev <laidlaw_dev@btinternet.com>
2025-09-30 13:28:59 +0100

    Add React ErrorBoundary

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,5 @@
-import { useTranslation } from 'react-i18next';
-import { DateTime } from 'luxon';
-import { Camera } from 'lucide-react';
-
-function App() {
-  const { t } = useTranslation();
-
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 text-center">
-      <div className="bg-primary text-on-primary p-1">
-        {t('application.name')}
-      </div>
-      <div className="bg-secondary text-on-secondary p-1">
-        {t('application.copyright', { year: DateTime.now().year })}
-      </div>
-      <div>
-        <Camera className="text-primary" />
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
