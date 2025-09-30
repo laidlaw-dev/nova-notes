9f43c48a1697032d2ac9b2f1624eb3817853d02b
laidlaw_dev <laidlaw_dev@btinternet.com>
2025-09-30 15:21:04 +0100

    Add AWS Cognito

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,43 @@
+import { useAuth } from 'react-oidc-context';
+
 const App = () => {
-  return <div></div>;
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = '1ifvfin3kr0hifc354juokfvjt';
+    const logoutUri = 'http://localhost:5173';
+    const cognitoDomain =
+      'https://eu-north-1crfjnnvjc.auth.eu-north-1.amazoncognito.com';
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+    </div>
+  );
 };
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,21 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import './i18n';
 import App from './App.tsx';
+import { AuthProvider } from 'react-oidc-context';
+
+const cognitoAuthConfig = {
+  authority:
+    'https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_crFJnnVJC',
+  client_id: '1ifvfin3kr0hifc354juokfvjt',
+  redirect_uri: 'http://localhost:5173',
+  response_type: 'code',
+  scope: 'phone openid email',
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>
 );
